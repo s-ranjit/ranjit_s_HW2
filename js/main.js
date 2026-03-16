e5310606686a483a1a26ac4f0d901bb661ed36f8
Situ Ranjit <situranjit6@gmail.com>
2026-03-16 12:39:16 -0400

    update initial value
    update initial value

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ import { Counter, StepCounter } from './modules/counter.js';
 import { GsapAnimation } from "./modules/gsap-animation.js";
 import { HamburgerMenu } from "./modules/ham-burger-menu.js";
 
-const container1 = new Counter("#counter-container1"); 
-const container2 = new Counter("#counter-container2", 4); // this will be default number
+const container1 = new Counter("#counter-container1", 3); // this will be default number
+const container2 = new Counter("#counter-container2"); 
 const newCounter = new Counter("#counterCon");
-const stepCounter = new StepCounter("#step-counter", 0, 5);
+const stepCounter = new StepCounter("#step-counter", 10, 5);
 
 GsapAnimation();
 HamburgerMenu();
